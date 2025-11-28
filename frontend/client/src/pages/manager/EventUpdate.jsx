94dea5e764a33a1b47e76917f6581c9c6bec8246
Nizar Shayan <nizar.shayan@mail.utoronto.ca>
2025-11-27 21:52:49 -0500

    Manager pages completed

--- frontend/client/src/pages/manager/EventUpdate.jsx
+++ frontend/client/src/pages/manager/EventUpdate.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+
 
 function renderField(label, key, type, form, setForm, editing, setEditing) {
   const isEditing = editing[key];
